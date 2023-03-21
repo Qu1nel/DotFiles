f880aafec9d976e384032216dac11456ca63c0e6
Ivan Covach <eeiprex@gmail.com>
2023-03-21 17:05:34 +0300

    [tools:bat] Add a setup file
    Also rename the configuration file to "config"

---
+++ bat/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+normalPath=$(dirname $0)
+
+if [ -z "$BAT_CONFIG_PATH" ]; then
+    mkdir -p "$HOME/.config/bat"
+    copyPath="$HOME/.config/bat"
+else
+    copyPath=$BAT_CONFIG_PATH
+fi
+
+cp "$normalPath/config" $copyPath

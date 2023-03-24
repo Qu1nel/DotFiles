e0f9974bdae9c8586b420c88ae8686ff74863531
Ivan Covach <eeiprex@gmail.com>
2023-03-24 19:39:15 +0300

    [chore](neofetch) update setup.sh for neofetch

--- neofetch/setup.sh
+++ neofetch/setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+USE_CONFIG="BEJKON2"  # 'PAPIRUS', 'CONFIG', 'ACENOSTER', 'BEJKON2'
+
 normalPath=$(dirname $0)
 
-cp -iv "$normalPath/config.conf" "$HOME/.config/neofetch"
+case $USE_CONFIG in
+    "CONFIG")
+        cp -iv "$normalPath/config.conf" "$HOME/.config/neofetch/config.conf"
+    ;;
+    "PAPIRUS")
+        cp -iv "$normalPath/papirus.conf" "$HOME/.config/neofetch/config.conf"
+    ;;
+    "ACENOSTER")
+        cp -iv "$normalPath/acenoster.conf" "$HOME/.config/neofetch/config.conf"
+    ;;
+    "BEJKON2")
+        cp -iv "$normalPath/bejkon2/config.conf" "$HOME/.config/neofetch/"
+        cp -iv "$normalPath/bejkon2/logo" "$HOME/.config/neofetch/"
+    ;;
+esac

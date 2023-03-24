fa19abcd1bc0bc668f3a7ebaeef859896b6f130f
Ivan Covach <eeiprex@gmail.com>
2023-03-24 18:37:00 +0300

    [chore](bash) add .bashrc update setup.sh
    A file has been added .bashrc with initial general settings and
    corresponding changes in 'setup.sh'.

--- bash/setup.sh
+++ bash/setup.sh
@@ -11,15 +11,19 @@ BASH_CONFIG_FOLDER="bash_config"
 MARK_HEADER="Bash configuration by Qu1nel"
 
 BASH_CONFIG_ROOT="$HOME/.config/$BASH_CONFIG_FOLDER"
+BASHRC_PATH="$HOME/.bashrc"
 
 ##  ============================================================================
 
 mkdir -p "$BASH_CONFIG_ROOT"
 
-BASHRC_PATH="$HOME/.bashrc"
+normalPath=$(dirname "$0")
+
 if [ ! -f "$BASHRC_PATH" ]; then
     cp "/etc/skel/.bashrc" "$BASHRC_PATH" # If .bashrc is not in the home directory, make it.
 fi
+
+cp -iv "$normalPath/.bashrc" "$BASHRC_PATH"
 
 CONFIG_CONTENT=(
     "paths.sh"
@@ -30,11 +34,9 @@ CONFIG_CONTENT=(
     "bash_prompts/"
 )
 
-normalPath=$(dirname "$0")
-
 # Copy the configuration files to the config folder.
 for file in ${CONFIG_CONTENT[@]}; do
-    cp -r "$normalPath/$file" "$BASH_CONFIG_ROOT"
+    cp -rv "$normalPath/$file" "$BASH_CONFIG_ROOT"
 done
 
 # DATA is the code that will be in '.bashrc'.
@@ -48,16 +50,7 @@ DATA=(
     'fi'
 )
 
-# Line number start of the configuration start line.
-startLineCode=$(($(grep -n "$MARK_HEADER" "$BASHRC_PATH" | sed 's/:.*//g')-1))
 lenthData=${#DATA[@]}
-
-# Deleting configuration startup lines from the file '.bashrc'.
-if [ $startLineCode ]; then
-    for i in $(seq 1 $lenthData); do
-        sed -i "${startLineCode}d" "$BASHRC_PATH"
-    done
-fi
 
 # Writing the configuration startup to a file '.bashrc'.
 for idx_code in ${!DATA[@]}; do

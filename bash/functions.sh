39bf6ebf84ad6b45ce7c970b66afbc667376a069
Ivan Covach <eeiprex@gmail.com>
2023-03-24 18:41:12 +0300

    [feature](bash) update 'functions.sh'
    Add a new function - ftext
    Searhes for text in all files in the current folder.

--- bash/functions.sh
+++ bash/functions.sh
@@ -29,10 +29,23 @@
         fi
     }
 
-#   mcd: Makes new Dir and jumps indide.
+#   mcd: Makes new Dir and jumps inside.
 #   ----------------------------------------------------
     mcd () {
         mkdir -pv "$1" &&
         cd "$1"
+    }
+
+#   ftext: Searhes for text in all files in the current folder.
+#   ----------------------------------------------------
+    ftext () {
+	    # -i case-insensitive
+	    # -i ignore binary files
+	    # -h causes filename to be printed
+	    # -r recursive search
+	    # -n causes line number to be printed
+	    # optional: -f treat search term as a literal, not a regular expression
+	    # optional: -l only print filenames and not the matching lines ex. grep -irl "$1" *
+        grep -IiHrn --color=always "$1" . | less -r
     }
 

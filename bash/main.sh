6d80373a921f77fadfe53258de42adc8558f2af8
Ivan Covach <eeiprex@gmail.com>
2023-05-01 23:13:07 +0300

    [chore:bash] Update functions.sh & main.sh
    Changes:
        bash/functions.sh
        bash/main.sh

--- bash/main.sh
+++ bash/main.sh
@@ -9,33 +9,36 @@
 #   Set paths
 #   ---------------------------------------------------------------------------
 
-    if [ -f "$BASH_CONFIG_ROOT/paths.sh" ]; then
-        source "$BASH_CONFIG_ROOT/paths.sh"
-    fi
+if [ -f "$BASH_CONFIG_ROOT/paths.sh" ]; then
+    # shellcheck disable=SC1091
+    source "$BASH_CONFIG_ROOT/paths.sh"
+fi
 
 #   Set aliases
 #   ---------------------------------------------------------------------------
 
-    if [ -f "$BASH_CONFIG_ROOT/aliases.sh" ]; then
-        source "$BASH_CONFIG_ROOT/aliases.sh"
-    fi
+if [ -f "$BASH_CONFIG_ROOT/aliases.sh" ]; then
+    # shellcheck disable=SC1091
+    source "$BASH_CONFIG_ROOT/aliases.sh"
+fi
 
 #   Define function
 #   ---------------------------------------------------------------------------
 
-    if [ -f "$BASH_CONFIG_ROOT/functions.sh" ]; then
-        source "$BASH_CONFIG_ROOT/functions.sh"
-    fi
+if [ -f "$BASH_CONFIG_ROOT/functions.sh" ]; then
+    # shellcheck disable=SC1091
+    source "$BASH_CONFIG_ROOT/functions.sh"
+fi
 
 #   Choice bash prompt
 #   ---------------------------------------------------------------------------
 
-    if [ -f "$BASH_CONFIG_ROOT/prompt.sh" ]; then
-        source "$BASH_CONFIG_ROOT/prompt.sh"
-    fi
+if [ -f "$BASH_CONFIG_ROOT/prompt.sh" ]; then
+    # shellcheck disable=SC1091
+    source "$BASH_CONFIG_ROOT/prompt.sh"
+fi
 
 #   Enable welcome tools
 #   ---------------------------------------------------------------------------
 
-    neofetch
-
+neofetch

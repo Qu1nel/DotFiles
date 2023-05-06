2556e51e5308fbfa6d694408e096f388d13d1d25
Ivan Covach <eeiprex@gmail.com>
2023-05-07 01:25:53 +0300

    [chore:tools] Update getwallpaper

--- tools/getwallpaper.sh
+++ tools/getwallpaper.sh
@@ -8,5 +8,5 @@ url_img="$(wget -O - https://www.reddit.com/r/wallpaper/ |\
 extension=${url_img: -3}
 wallpaper="background.$extension"
 
-wget -O "$wallpaper" "$url_img"
-gsettings set org.gnome.desktop.background picture-uri "$PWD/$wallpaper"
+wget -O "/tmp/$wallpaper" "$url_img"
+gsettings set org.gnome.desktop.background picture-uri "/tmp/$wallpaper"

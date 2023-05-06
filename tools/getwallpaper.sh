15e2895af30537074868db0c3f86d0f72655be64
Ivan Covach <eeiprex@gmail.com>
2023-05-07 01:11:06 +0300

    [tools:add] Add new program - getwallpaper.sh
    Program 'getwallpaper' randomly selects wallpaper with
    https://www.reddit.com/r/wallpaper / and puts it on the desktop.
    
    Works only on gnome 3+
    
    Changes:
        tools/getwallpaper.sh

---
+++ tools/getwallpaper.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+url_img="$(wget -O - https://www.reddit.com/r/wallpaper/ |\
+    grep -oP 'https?:\/\/preview\.redd\.it\/[^"\/]+(?=\?)' | sed 's/preview/i/gm' |\
+    sort | uniq | sort | uniq | sort |\
+    shuf -n 1)"
+
+extension=${url_img: -3}
+wallpaper="background.$extension"
+
+wget -O "$wallpaper" "$url_img"
+gsettings set org.gnome.desktop.background picture-uri "$PWD/$wallpaper"

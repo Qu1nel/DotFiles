e0cbbcf7dfc1fc61d9c5e9b170cb47e10b41b3ff
Ivan Covach <eeiprex@gmail.com>
2023-05-13 00:33:45 +0300

    [chore:neofetch] Update config
    Files:
        acenoster.conf
        bejkon2/config.conf
        config.conf
        papirus.conf
        setup.sh
    
        ADD: bejkon2/logo1
        ADD: bejkon2/logo2
        ADD: bejkon2/logo3
        ADD: bejkon2/logo4
        ADD: bejkon2/logo5

--- neofetch/setup.sh
+++ neofetch/setup.sh
@@ -4,18 +4,18 @@ USE_CONFIG="BEJKON2"  # 'PAPIRUS', 'CONFIG', 'ACENOSTER', 'BEJKON2'
 
 currentPath="${0%/*}"
 
-case $USE_CONFIG in
+case "$USE_CONFIG" in
     "CONFIG")
         cp -iv "$currentPath/config.conf" "$HOME/.config/neofetch/config.conf"
-    ;;
+        ;;
     "PAPIRUS")
         cp -iv "$currentPath/papirus.conf" "$HOME/.config/neofetch/config.conf"
-    ;;
+        ;;
     "ACENOSTER")
         cp -iv "$currentPath/acenoster.conf" "$HOME/.config/neofetch/config.conf"
-    ;;
+        ;;
     "BEJKON2")
         cp -iv "$currentPath/bejkon2/config.conf" "$HOME/.config/neofetch/"
         cp -iv "$currentPath/bejkon2/logo" "$HOME/.config/neofetch/"
-    ;;
+        ;;
 esac

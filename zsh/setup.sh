ab6e2f72527bc6aedd6497afa8861f96408f3bc7
Ivan Covach <eeiprex@gmail.com>
2023-06-15 20:38:55 +0300

    [chore:zsh] Add new file `.zshrc`
    Files:
        [ch] setup.sh
        [a] .zshrc

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -18,6 +18,8 @@ mkdir -pv "$ZSH_CONFIG_ROOT"
 
 currentPath="${0%/*}"
 
+cp -iv "$currentPath/.zshrc" "$ZSHRC_PATH"
+
 CONFIG_CONTENT=(
     "paths.sh"
     "aliases.sh"
@@ -30,8 +32,7 @@ for file in "${CONFIG_CONTENT[@]}"; do
     cp -vi "$currentPath/$file" "$ZSH_CONFIG_ROOT"
 done
 
-# TODO:
-# echo 'export ZSH_CONFIG_ROOT="$HOME/.config/zsh_config"' >> "$ZSHRC_PATH"
-# echo 'if [ -f "$ZSH_CONFIG_ROOT/main.zsh" ]; then' >> "$ZSHRC_PATH"
-# echo '    source "$ZSH_CONFIG_ROOT/main.zsh"' >> "$ZSHRC_PATH"
-# echo 'fi' >> "$ZSHRC_PATH"
+echo 'export ZSH_CONFIG_ROOT="$HOME/.config/'"$ZSH_CONFIG_FOLDER"'"' >> "$ZSHRC_PATH"
+echo 'if [ -f "$ZSH_CONFIG_ROOT/main.zsh" ]; then' >> "$ZSHRC_PATH"
+echo '    source "$ZSH_CONFIG_ROOT/main.zsh"' >> "$ZSHRC_PATH"
+echo 'fi' >> "$ZSHRC_PATH"

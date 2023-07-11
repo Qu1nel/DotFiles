cf342f4692eaa4f5cf9a665fb3c703513310f0a0
Ivan Kovach <eeiprex@gmail.com>
2023-07-11 22:36:07 +0300

    [fix#scripts] Fixed find files options
    Update scripts:
    
        * concat_img.sh

--- scripts/concat_img.sh
+++ scripts/concat_img.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-array=$(find . -type f -regex "\.\/[0-9]+\.png" -printf "%f ")
+array=$(find . -type f -regex "\.\/[0-9]+\.png" | sort | sed "s|./||g" | xargs printf "%s ")
 
 echo "convert ${array}-append $(date +%s).png"
 convert ${array} -append "$(date +%s).png"

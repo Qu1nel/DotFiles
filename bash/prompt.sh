ac68bbf747fc38a156719d6459628c330f21e2e0
Ivan Kovach <eeiprex@gmail.com>
2023-07-27 19:14:12 +0300

    refact

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -6,27 +6,19 @@
 ##
 ##  ===========================================================================
 
-SELECTED_PROMT="POWERLINE" # Can be: CUSTOM, SYNTH, STARSHIP, POWERLINE
+SELECTED_PROMT="POWERLINE" # Can be: CUSTOM, STARSHIP, POWERLINE
 
 case "$SELECTED_PROMT" in
     "CUSTOM")
-        # shellcheck disable=SC1091
         source "$BASH_CONFIG_ROOT/bash_prompts/custom/custom_prompt.sh"
         ;;
-    "SYNTH")
-        # shellcheck disable=SC1091
-        source "$BASH_CONFIG_ROOT/bash_prompts/synth/synth_prompt.sh"
-        ;;
     "STARSHIP")
-        # shellcheck disable=SC1091
         source "$BASH_CONFIG_ROOT/bash_prompts/starship/starship_prompt.sh"
         ;;
     "POWERLINE")
-        # shellcheck disable=SC1091
         source "$BASH_CONFIG_ROOT/bash_prompts/powerline/powerline.sh"
         ;;
     *)
-        # shellcheck disable=SC1091
         source "$BASH_CONFIG_ROOT/bash_prompts/default/default_prompt.sh"
         ;;
 esac

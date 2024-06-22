9febddb778039a06f5d0bc5a0ad56e0a77d5d57b
Ivan Kovach <eeiprex@gmail.com>
2024-06-22 14:57:07 +0300

    [chore#nvim] Update `astroui.lua` add transparent

--- nvim/config/lua/plugins/astroui.lua
+++ nvim/config/lua/plugins/astroui.lua
@@ -6,7 +6,18 @@ return {
     colorscheme = "gruvbox",
     highlights = {
       init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+        Normal = { guibg = nil, ctermbg = nil, bg = nil },
+        NormalNC = { bg = nil, ctermbg = nil },
+        CursorColumn = { cterm = nil, ctermbg = nil, ctermfg = nil },
+        CursorLine = { cterm = {}, ctermbg = nil, ctermfg = nil },
+        CursorLineNr = { cterm = {}, ctermbg = nil, ctermfg = nil },
+        SignColumn = {},
+        StatusLine = {},
+        NeoTreeNormal = { bg = nil, ctermbg = nil },
+        NeoTreeNormalNC = { bg = nil, ctermbg = nil },
+        require("notify").setup {
+          background_colour = "#000000",
+        },
       },
       astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },

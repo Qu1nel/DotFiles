7ce8f54e6b19236c3c268e49b54441b09064af34
Ivan Kovach <eeiprex@gmail.com>
2024-06-22 14:11:57 +0300

    [refact#tmux] Update config for `tmux` and add setup for it

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 currentPath="${0%/*}"
-file=".tmux.conf"
 
-cp -vi "$currentPath/$file" "$HOME/"
+config_folder=".tmux"
+
+cp -vri "$currentPath/$config_folder" "$HOME"
+cp -v "$currentPath/$config_folder/.tmux.conf.local" "$HOME"
+
+ln -s -f "$currentPath/$config_folder/.tmux.conf" "$HOME"
+

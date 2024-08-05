6e5acdfe5d9a4e8a7dee53bfccd9ecabeaa5ca78
Ivan Kovach <eeiprex@gmail.com>
2024-08-05 15:20:17 +0300

    [chore:zsh] Remove ssh-agent part on startup

--- zsh/paths.sh
+++ zsh/paths.sh
@@ -46,21 +46,7 @@ function start_agent {
     /usr/bin/ssh-add;
 }
 
-# Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ]; then
-    . "${SSH_ENV}" > /dev/null
-    #ps ${SSH_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
-    }
-else
-    start_agent;
-fi
-
-if [ -z "$SSH_AUTH_SOCK" ] ; then
-  eval `ssh-agent -s`
-  ssh-add
-fi
+start_agent;
 
 #   Command history
 #   ---------------------------------------------------------------------------
